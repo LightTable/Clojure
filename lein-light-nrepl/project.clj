c6199bea9a93107818c1e7fc6ef857506fff86c3
Chris Granger <ibdknox@gmail.com>
2014-05-19 19:06:59 -0700

    inc all the version numbers, build, 0.0.12
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- lein-light-nrepl/project.clj
+++ lein-light-nrepl/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-light-nrepl "0.0.17"
+(defproject lein-light-nrepl "0.0.18"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/clojure "0.0.11"
+(defproject com.lighttable/clojure "0.0.12"
   :description "Clojure plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
