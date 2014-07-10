d9ea2e5dc7a1b5254544cbaaf5ba4b4802aa43e4
Chris Granger <ibdknox@gmail.com>
2014-07-10 09:30:42 -0700

    0.0.13
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/clojure "0.0.12"
+(defproject com.lighttable/clojure "0.0.13"
   :description "Clojure plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
