4e7091eebbf87af438b8f90fef600ee6eff77f74
Gabriel Horner <gabriel.horner@gmail.com>
2014-11-10 08:37:09 -0500

    Bump all to 0.1.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/clojure "0.0.14"
+(defproject com.lighttable/clojure "0.1.0"
   :description "Clojure plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
