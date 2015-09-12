bd655df2fb8ea811f72c5d6fdb09d23d58fcad87
Gabriel Horner <gabriel.horner@gmail.com>
2015-09-12 16:58:30 -0400

    Abort invalid clj + cljs combos. Misc fixes and cleanup
    * clojure-1-7-or-greater was incorrect for 2.0.0. Made existing fn
      at-least-version? reusable
    * Remove one unused and one unnecessary fn
    * Explicitly fail for invalid version strings instead of NPE

--- runner/src/leiningen/light_nrepl.clj
+++ runner/src/leiningen/light_nrepl.clj
@@ -3,50 +3,41 @@
   (:require [leiningen.core.project :as lp]
             [leiningen.repl :as repl]
             [clojure.string :as string]
-            [fs.core :as fs])
-  (:use [leiningen.core.eval :only [eval-in-project]]))
+            [fs.core :as fs]))
 
 (defn parse-version [ver]
-  (when ver
-    (let [[major minor patch] (string/split ver #"\.")
-          [patch extra] (string/split patch #"-")]
-      {:major (Integer. major)
-       :minor (Integer. minor)
-       :patch (Integer. patch)})))
+  (assert (and ver (re-find #"^\d+\.\d+\.\d+" ver))
+          (str "Invalid version number: " (pr-str ver) ". Must be in format X.X.X"))
+  (let [[major minor patch] (string/split ver #"\.")
+        [patch extra] (string/split patch #"-")]
+    {:major (Integer. major)
+     :minor (Integer. minor)
+     :patch (Integer. patch)}))
 
-(defn valid-clojure? [ver]
+(defn at-least-version? [ver min-version]
+  (let [{:keys [major minor patch]} (parse-version ver)]
+    (or (> major (:major min-version))
+        (and (= major (:major min-version)) (> minor (:minor min-version)))
+        (and (= major (:major min-version)) (= minor (:minor min-version)) (>= patch (:patch min-version))))))
+
+(defn maintained-clojure-version?
+  "Determine if Clojure version is supported for latest middleware.
+  Return true for nil since there is no version and thus we are providing one"
+  [ver]
   (if-not ver
     true
-    (let [{:keys [major minor patch]} (parse-version ver)]
-      ;;do this by negation. It's an invalid version if any of the
-      ;;following are true
-      (not
-       (or (< major 1)
-           (and (= major 1) (< minor 5))
-           (and (= major 1) (= minor 5) (< patch 1)))))))
+    ;; 1.7.0 candidates conflict with 1.7.0
+    (and (not (.startsWith ver "1.7.0-"))
+         (at-least-version? ver {:major 1 :minor 7 :patch 0}))))
 
-(defn proj->name [proj]
-  (str (:name proj) " " (:version proj)))
-
-(defn clojure-1-7-or-greater? [ver]
-  (when ver
-    (let [{:keys [major minor patch]} (parse-version ver)]
-      (and (>= major 1)
-           (>= minor 7)
-           (>= patch 0)
-           ;; 1.7.0 candidates conflict with 1.7.0
-           (not (.startsWith ver "1.7.0-"))))))
-
-(defn find-clojure-version [proj]
-  (let [deps (:dependencies proj)]
-    (second (first (filter #(= (first %) 'org.clojure/clojure) deps)))))
-
-(defn prep [project name]
+(defn prep
+  "Build a project map for the repl with LT middleware injected"
+  [project name clj-version]
   (let [init `(swap! lighttable.nrepl.core/my-settings merge {:name ~(or name (str (:name project) " " (:version project))) :project (quote ~project)})
         init (if-let [cur-init (-> project :repl-options :init)]
                (list 'do cur-init init)
                init)
-        lein-light-version (if (clojure-1-7-or-greater? (find-clojure-version project))
+        lein-light-version (if (maintained-clojure-version? clj-version)
                              ;; Maintained lein-light-nrepl
                              "0.2.0"
                              ;; Deprecated/unmaintained lein-light-nrepl
@@ -54,31 +45,43 @@
         profile {:dependencies [['lein-light-nrepl/lein-light-nrepl lein-light-version]
                                 '[org.clojure/tools.reader "0.9.1"]]
                  :repl-options {:nrepl-middleware ['lighttable.nrepl.handler/lighttable-ops]
-                                 :init (with-meta init {:replace true})}}
+                                :init (with-meta init {:replace true})}}
         project (lp/merge-profiles project [profile])]
     (println "final project: " project)
-      project))
+    project))
 
-(defn light
-  "Start a Light Table client for this project"
-  [project & [name]]
-  (when-not (valid-clojure? (find-clojure-version project))
+(defn abort-unsupported-versions [clj-version cljs-version]
+  (when (and clj-version (not (at-least-version? clj-version {:major 1 :minor 5 :patch 1})))
     (binding [*out* *err*]
       (println "Light Table requires Clojure Version 1.5.1 or higher")
       (System/exit 0)))
-  (try
-    (repl/repl (prep project name) ":headless")
-    (catch Exception e
-      (.printStackTrace e)
-      (System/exit 1))))
+  (when (and cljs-version (at-least-version? cljs-version {:major 0 :minor 0 :patch 2341})
+             (not (maintained-clojure-version? clj-version)))
+    (binding [*out* *err*]
+      (println "Light Table requires Clojure Version >= 1.7.0 for ClojureScript versions >= 0.0-2341")
+      (System/exit 0))))
 
-(defn check-project [path]
-  (when (fs/exists? path)
-    path))
+(defn find-dependency-version [proj dep]
+  (let [deps (:dependencies proj)]
+    (second (first (filter #(= (first %) dep) deps)))))
+
+(defn light
+  "Start a Light Table client for this project"
+  [project name]
+  (let [clj-version (find-dependency-version project 'org.clojure/clojure)
+        cljs-version (some-> (find-dependency-version project 'org.clojure/clojurescript)
+                             ;; Make 0.0- conform to standard patch versions
+                             (string/replace "0.0-" "0.0."))]
+    (abort-unsupported-versions clj-version cljs-version)
+    (try
+      (repl/repl (prep project name clj-version) ":headless")
+      (catch Exception e
+        (.printStackTrace e)
+        (System/exit 1)))))
 
 (defn -main [& [name]]
   (let [path (str (fs/absolute-path fs/*cwd*) "/project.clj")]
-    (if (check-project path)
+    (if (fs/exists? path)
       (light (lp/init-project (lp/read path)) name)
       (binding [*out* *err*]
         (println "Could not find project.clj file at: " path)))))
