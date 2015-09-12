07de3f16672e2b0a4030ff22166740dd038f295a
Gabriel Horner <gabriel.horner@gmail.com>
2015-09-12 17:53:27 -0400

    Bump runner to clojure version we support for eval

--- runner/project.clj
+++ runner/project.clj
@@ -1,9 +1,8 @@
 (defproject lein-light "0.1.3"
   :description "FIXME: write description"
-  :url "http://github.com/kodowa/lein-light"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [leiningen "2.3.4"
                   :exclusions [stencil]]
                  [fs "1.3.3"]]
