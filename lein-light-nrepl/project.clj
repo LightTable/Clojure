8ad0d0d3c385233203ce392d33493227d3833712
Camilo Roca <carocad@unal.edu.co>
2016-04-10 13:13:29 +0200

    replaced clj-stacktrace by aviso/pretty. exception formatting code refactored accordingly

--- lein-light-nrepl/project.clj
+++ lein-light-nrepl/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-light-nrepl "0.3.2"
+(defproject lein-light-nrepl "0.3.3"
   :description "nrepl client for Light Table clj and cljs eval."
   :url "https://github.com/LightTable/Clojure/tree/master/lein-light-nrepl"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                  [org.clojure/tools.nrepl "0.2.10"]
                  [commons-io/commons-io "2.4"]
                  [org.clojure/tools.reader "0.9.2"]
-                 [clj-stacktrace "0.2.8"]
+                 [io.aviso/pretty "0.1.25"]
                  [org.clojure/clojurescript "0.0-3308"
                    :exclusions [org.apache.ant/ant]]
                  [clojure-complete "0.2.4"]]
